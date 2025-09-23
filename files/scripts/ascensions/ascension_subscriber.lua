934815fb5944dc803beb686a66be04000bbcc4be
den3606 <den3606.dev@gmail.com>
2025-09-23 15:47:53 +0900

    fix: add end / not implement will cause error

--- files/scripts/ascensions/ascension_subscriber.lua
+++ files/scripts/ascensions/ascension_subscriber.lua
@@ -6,18 +6,22 @@ ascension.name = "none"
 ascension.description = "none"
 
 function ascension:on_activate()
-  print("on_activate is not implement")
+  error("on_activate is not implement")
+end
+
+function ascension:on_spawn_shop_item()
+  error("on_spawn_shop_item is not implement")
 end
 
 function ascension:on_world_pre_update()
-  print("on_world_pre_update is not implement")
+  error("on_world_pre_update is not implement")
 end
 function ascension:on_player_spawn()
-  print("on_player_spawn is not implement")
+  error("on_player_spawn is not implement")
 end
 
 function ascension:on_enemy_spawn(_enemy)
-  print("on_enemy_spawn is not implement")
+  error("on_enemy_spawn is not implement")
 end
 
 function ascension:should_unlock_next()
