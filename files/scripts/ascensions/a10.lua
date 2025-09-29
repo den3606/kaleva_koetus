7036d682481339a6f12a9c897792ce23023512e0
den3606 <den3606.dev@gmail.com>
2025-09-30 01:52:06 +0900

    :hammer: a10 buff

--- files/scripts/ascensions/a10.lua
+++ files/scripts/ascensions/a10.lua
@@ -11,7 +11,7 @@ local ascension = setmetatable({}, { __index = AscensionBase })
 
 local log = Logger:new("a10.lua")
 
-local WAIT_FRAME = 60 * 60 * 20
+local WAIT_FRAME = 60 * 60 * 15
 local DELAY_FRAME = 60 * 2
 
 ascension.level = 10
