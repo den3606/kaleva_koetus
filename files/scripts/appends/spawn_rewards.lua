4a6c0204c714884fb0cad99e2b75e66c822d6ad9
den3606 <den3606.dev@gmail.com>
2025-10-21 02:10:56 +0900

    :sparkles: add a20 reward

---
+++ files/scripts/appends/spawn_rewards.lua
@@ -0,0 +1,6 @@
+local _spawn_rewards = spawn_rewards
+-- selene: allow(unused_variable)
+function spawn_rewards(x, y)
+  _spawn_rewards(x, y)
+  local _ = EntityLoad("mods/kaleva_koetus/files/entities/animals/boss_centipede/rewards/reward_a20.xml", x + 40, y - 50)
+end
--- files/scripts/ascensions/a20.lua
+++ files/scripts/ascensions/a20.lua
@@ -21,6 +21,10 @@ function ascension:on_activate()
     "data/scripts/biomes/mountain/mountain_floating_island.lua",
     "mods/kaleva_koetus/files/scripts/appends/mountain_floating_island.lua"
   )
+  ModLuaFileAppend(
+    "data/entities/animals/boss_centipede/rewards/spawn_rewards.lua",
+    "mods/kaleva_koetus/files/scripts/appends/spawn_rewards.lua"
+  )
 end
 
 function ascension:on_boss_died()
