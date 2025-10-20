809e90e27db21cfdb1ea2df0ebaf20da761ebaef
den3606 <den3606.dev@gmail.com>
2025-10-21 01:19:09 +0900

    :hammer: sampo can get without friend at a17

--- files/scripts/ascensions/a17_sampo_pickup_option.lua
+++ files/scripts/ascensions/a17_sampo_pickup_option.lua
@@ -1,14 +1,16 @@
 local _ = dofile_once("mods/kaleva_koetus/files/scripts/lib/utils/player.lua")
 
-local sampo_entity_id = GetUpdatedEntityID()
+-- NOTE:
 -- プレイヤーの座標を取って、そこから200以内にfriendがいなければ、無効化してテキストを出す
 local plyer_entity_id = GetPlayerEntity()
 local x, y = EntityGetTransform(plyer_entity_id)
 local friend_entity_id = EntityGetInRadiusWithTag(x, y, 200, "kk_a17_friend")[1]
 local exist_friend = friend_entity_id and friend_entity_id ~= 0
 
+local sampo_entity_id = GetUpdatedEntityID()
 local item_component_id = EntityGetFirstComponentIncludingDisabled(sampo_entity_id, "ItemComponent")
-if exist_friend or ModSettingGet("kaleva_koetus.a20_dead_boss") or tonumber(SessionNumbersGetValue("NEW_GAME_PLUS_COUNT")) or 0 > 0 then
+
+if exist_friend or ModSettingGet("kaleva_koetus.a20_dead_boss") or ((tonumber(SessionNumbersGetValue("NEW_GAME_PLUS_COUNT")) or 0) > 0) then
   ComponentSetValue2(item_component_id, "is_pickable", true)
 else
   local announced = GlobalsGetValue("kaleva_koetus_a17_boss_announced", "false") == "true"
