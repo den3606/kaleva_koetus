87bdb1d315b9e766772c0ad2575f93f7d42f75a3
SlpAus <102158919+SlpAus@users.noreply.github.com>
2025-11-04 10:05:32 +0800

    :hammer: fix(a17): Improve follower logic and radar reliability

--- files/scripts/ascensions/a17_friend_movement.lua
+++ files/scripts/ascensions/a17_friend_movement.lua
@@ -1,27 +1,73 @@
 local _ = dofile_once("mods/kaleva_koetus/files/scripts/lib/utils/player.lua")
 local _ = dofile_once("data/scripts/lib/utilities.lua")
 
-local entity_id = GetUpdatedEntityID()
-local x, y = EntityGetTransform(entity_id)
-local px, py = x, y
-local owner_id = GetPlayerEntity()
-
-local comps = EntityGetComponent(entity_id, "VariableStorageComponent")
+local max_distance = 200
 local swaying = true
 
+local entity_id = GetUpdatedEntityID()
+
+local radar_x, radar_y = EntityGetFirstHitboxCenter(entity_id)
+GlobalsSetValue("kaleva_koetus.a17_friend_last_x", tostring(radar_x))
+GlobalsSetValue("kaleva_koetus.a17_friend_last_y", tostring(radar_y))
+
+local x, y = EntityGetTransform(entity_id)
+local px, py
+
+local comps = EntityGetComponent(entity_id, "VariableStorageComponent")
 if comps ~= nil then
+  local owner_id
+  local owner_id_comp, target_x_comp, target_y_comp
   for _, v in ipairs(comps) do
     local name = ComponentGetValue2(v, "name")
-
     if name == "owner_id" then
-      px, py = EntityGetTransform(owner_id)
+      owner_id_comp = v
+    elseif name == "target_x" then
+      target_x_comp = v
+    elseif name == "target_y" then
+      target_y_comp = v
+    end
 
-      if (px == nil) or (py == nil) then
-        px, py = x, y
+    if owner_id_comp ~= nil then
+      local temp_owner_id = ComponentGetValue2(owner_id_comp, "value_int")
+      if EntityGetIsAlive(temp_owner_id) == true then
+        owner_id = temp_owner_id
+        px, py = EntityGetTransform(owner_id)
+      end
+    end
+
+    if owner_id == nil then
+      local temp_owner_id = GetPlayerEntity()
+      if temp_owner_id ~= nil and EntityGetIsAlive(temp_owner_id) == true then
+        owner_id = temp_owner_id
+        px, py = EntityGetTransform(owner_id)
+        if owner_id_comp ~= nil then
+          ComponentSetValue2(owner_id_comp, "value_int", owner_id)
+        end
+      end
+    end
+
+    if owner_id == nil then
+      px = target_x_comp ~= nil and ComponentGetValue2(target_x_comp, "value_float") or x
+      py = target_y_comp ~= nil and ComponentGetValue2(target_y_comp, "value_float") or y
+    else
+      if target_x_comp ~= nil then
+        ComponentSetValue2(target_x_comp, "value_float", px)
+      end
+      if target_y_comp ~= nil then
+        ComponentSetValue2(target_y_comp, "value_float", py)
       end
     end
   end
 end
+
+-- selene: allow(undefined_variable)
+local dist = get_distance(x, y, px, py)
+if dist >= max_distance then
+  PhysicsSetStatic(entity_id, true)
+  return
+end
+
+PhysicsSetStatic(entity_id, false)
 
 local cvx, cvy = 0, 0
 local physcomp = EntityGetFirstComponent(entity_id, "PhysicsBodyComponent")
@@ -40,7 +86,7 @@ end
 -- selene: allow(undefined_variable)
 local dir = get_direction(x, y, px, py)
 -- selene: allow(undefined_variable)
-local dist = math.min(get_distance(x, y, px, py), 32)
+dist = math.min(get_distance(x, y, px, py), 32)
 
 local vel_x = 0 - (math.cos(dir) * dist)
 local vel_y = 0 - (0 - math.sin(dir) * dist)
@@ -54,16 +100,3 @@ if ((y > py) and (cvy > 0)) or ((y < py) and (cvy < 0)) then
 end
 
 PhysicsApplyForce(entity_id, vel_x, vel_y)
-
-if owner_id ~= 0 then
-  x, y = EntityGetTransform(entity_id)
-  local ox, oy = EntityGetTransform(owner_id)
-  dist = math.abs(x - ox) + math.abs(y - oy)
-
-  if dist < 300 then
-    PhysicsApplyForce(entity_id, vel_x, vel_y)
-    PhysicsSetStatic(entity_id, false)
-  else
-    PhysicsSetStatic(entity_id, true)
-  end
-end
