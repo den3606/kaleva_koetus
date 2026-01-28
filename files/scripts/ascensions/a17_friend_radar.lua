3dcb5a414856d805210bbdb7dbcb566b636eccca
SlpAus <102158919+SlpAus@users.noreply.github.com>
2026-01-29 03:13:30 +0800

    :hammer: fix(a17): Friend radar check boss death flag via Globals

--- files/scripts/ascensions/a17_friend_radar.lua
+++ files/scripts/ascensions/a17_friend_radar.lua
@@ -1,7 +1,14 @@
 local _ = dofile_once("data/scripts/lib/utilities.lua")
 
+local EventDefs = dofile_once("mods/kaleva_koetus/files/scripts/event_hub/event_types.lua")
+
+local EventTypes = EventDefs.Types
+local AscensionTags = EventDefs.Tags
+
+local a20_boss_died_key = AscensionTags.A1 .. EventTypes.BOSS_DIED
+
 local entity_id = GetUpdatedEntityID()
-if ModSettingGet("kaleva_koetus.a20_dead_boss") or ((tonumber(SessionNumbersGetValue("NEW_GAME_PLUS_COUNT")) or 0) > 0) then
+if GlobalsGetValue(a20_boss_died_key, "0") == "1" or ((tonumber(SessionNumbersGetValue("NEW_GAME_PLUS_COUNT")) or 0) > 0) then
   local lua_component_id = GetUpdatedComponentID()
   EntityRemoveComponent(entity_id, lua_component_id)
   return
